5b5fabc9a1c33af8834cc2ef0304c49b52688c01
yuya-kodera <kodera.dakara@gmail.com>
2016-12-19 14:13:10 +0000

    circleとeventの入れ子ルーティング2

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :circles do
-  	resources :events
+  	resources :events, only: [:create, :edit, :update, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
