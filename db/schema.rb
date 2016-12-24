887cf396a680da732a89e62cc65e80fedbab65ab
Arima <anthorse_koki0703@ozzio.jp>
2016-12-24 05:36:46 +0000

    サークルジョインの変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161212124928) do
+ActiveRecord::Schema.define(version: 20161224052051) do
 
   create_table "circels", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20161212124928) do
   end
 
   create_table "circle_joins", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "circle_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -36,6 +38,7 @@ ActiveRecord::Schema.define(version: 20161212124928) do
   create_table "comments", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "event_joins", force: :cascade do |t|
@@ -46,6 +49,7 @@ ActiveRecord::Schema.define(version: 20161212124928) do
   create_table "events", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
