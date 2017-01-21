4aa999063754f27a13920b1b427a0573ca1e8b7c
Arima <anthorse_koki0703@ozzio.jp>
2017-01-21 13:20:24 +0000

    微調整

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,13 @@ Rails.application.routes.draw do
 
   resources :events
 
+  resources :users, only: [:index, :show] do
+    get :circle_joins, on: :member
+  end
+
+  resources :circles do
+    resource :circle_joins, only: [:create, :destroy]
+  end
+  root 'circles#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
